dcdfd522808a32e246a8c52d03c83cc9b55b44ba
Jake Choynowski <Safte-R@ibrinc.org>
2023-09-01 14:33:27 -0400

    Completed SAFTE Missing Sleep function
    Updated package description info, etc.

--- R/SAFTE_missing_sleep.R
+++ R/SAFTE_missing_sleep.R
@@ -1,18 +1,31 @@
-##########################  SAFTE Missing Sleep #################################
-####                                                                         ####
-###   This takes the raw, properly formatted data uploaded by the user and    ###
-####     searches for any dates that do not include Sleep. This could be     ####
-###      helpful in identifying missing nights of sleep data which can        ###
-####     dramatically affect the calculations in the SAFTE model.            ####
-###                                                                           ###
-####     This will output a table listing all 24 hour periods that do not    ####
-###      have an instance of sleep.                                           ###
-###                                                                          ####
-#################################################################################
-
-#### REQUIRED LIBRARIES:
-# library(tidyverse)
-# library(lubridate)
+#' SAFTE Missing Sleep
+#'
+#' This function takes the raw, properly formatted data uploaded by the user and
+#' searches for any dates that do not include "Sleep". This could be helpful in
+#' identifying missing night of sleep data which can dramatically affect the
+#' calculations in the SAFTE model.
+#'
+#' Properly formatted data at a minimum has a single column for
+#' sleep identification, a single column for start datetimes for bedtime, and a
+#' single column for end dateimes for waketimes. It can also include work, test,
+#' and crewing identified periods within the same columns.
+#'
+#' This is not for EPOCH by EPOCH data or data already divided into 1 minute
+#' epochs.
+#'
+#' @param dataset Name of the properly formatted dataset uploaded into the R environment by the user (Required)
+#' @param subject_id Identifier of the subject/participant/patient. (Required)
+#' @param split_time #the hour (0-23) at which the user wants to split the day. Deafults to noon ("12"). (Required)
+#' @param sleep_id Marker ID for a sleep period (usually "Sleep" or "sleep"). Defaults to "Sleep"#'
+#' @param obs_start Oberservation start time (i.e. beginning of the study). Formatted as 'YYYY-mm-dd HH:MM' (Required)
+#' @param obs_end Observation end time (i.e. end of the study). Cannot be more than 21 days from the obs_start. Formatted as 'YYYY-mm-dd HH:MM' (Required)
+#'
+#' @returns This will output a table listing all 24 hour periods that do not have an instance of sleep.
+#'
+#' @import tidyverse
+#' @import lubridate
+#'
+#' @export
 
 
 SAFTE_missing_sleep<-
